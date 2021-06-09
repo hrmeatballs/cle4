f81e4a46c0de0a0544c889be2d5b533679141995
Lars Beukenkamp <lars@beukenkamp.net>
2021-06-09 14:53:35 +0200

    Added touchscreen and
    Made sure you can shoot using touchscreen. Also made aiming more presisely.

--- dev/classes/GameObj/bubbleShooter.ts
+++ dev/classes/GameObj/bubbleShooter.ts
@@ -8,22 +8,27 @@ export class bubbleShooter {
 
     constructor() {
         console.log('Created bubble shooter')
-        //add event listener on mousemove
+        //add event listener on mouse
         document.addEventListener('mousemove', (e : MouseEvent) => this.onMouseMove(e))
+        document.addEventListener('mousedown', (e : MouseEvent) => this.onMouseMove(e))
         document.addEventListener('click', (e : MouseEvent) => this.onMouseClick(e))
+
+        //add event listeners on touch
+        document.addEventListener('touchmove', (e : TouchEvent) => this.onTouchMove(e))
+        document.addEventListener('touchstart', (e : TouchEvent) => this.onTouchMove(e))
+        document.addEventListener('touchend', (e : TouchEvent) => this.onTouchEnd(e))
 
         //creating canvas with width and height same as window width and height (fullscreen)
         this.canvas = document.createElement('canvas')
-        this.canvas.style.height = '99vh'
-        this.canvas.style.width = '99vw'
+        this.canvas.style.height = '100vh'
+        this.canvas.style.width = '100vw'
+        this.canvas.style.position = 'fixed'
         document.body.appendChild(this.canvas)
 
         this.createTile()
     }
 
     private createTile() {
-        //resetting gamestate
-        this.gameState = 'playing'
         //resetting position
         this.player = this.getPlayerPos()
 
@@ -39,7 +44,7 @@ export class bubbleShooter {
         this.playerDiv.style.color = 'white'
         document.body.appendChild(this.playerDiv)
 
-        
+        this.gameState = 'aiming'
     }
 
     //return position of the place where the ball is going to shoot from
@@ -47,7 +52,7 @@ export class bubbleShooter {
         return {
             x: window.innerWidth/2 - 50,
             y: window.innerHeight - 100,
-            angle: 0
+            angle: 90
         }
     }
 
@@ -61,18 +66,15 @@ export class bubbleShooter {
         return angle * (Math.PI / 180);
     }
 
-    //function activated by mousemove eventlistener
-    private onMouseMove(e : MouseEvent) {
-
-        if(this.gameState == 'playing') {
-            //rotating the tile facing to the mouse
-            this.playerDiv.style.transform = `rotate(${this.degToRad(this.player.angle)}rad)`
+    //function activated by touchmove eventlistener
+    private onTouchMove(e : TouchEvent) {
+        if(this.gameState == 'aiming') {
         
             //get array with x,y (mouse position)
-            let pos = this.getMousePos(this.canvas, e);
+            let pos = this.getTouchPos(this.canvas, e);
             
             //get the angle from a certain point to the mouse in degrees
-            let mouseangle = this.radToDeg(Math.atan2(this.player.y - pos.y, this.player.x - pos.x))
+            let mouseangle = this.radToDeg(Math.atan2(this.player.y - pos.y + 50, this.player.x - pos.x + 50))
 
             //making sure you can't shoot down
             if (mouseangle < 0) {
@@ -99,6 +101,50 @@ export class bubbleShooter {
         }
     }
 
+    //function activated by mousemove eventlistener
+    private onMouseMove(e : MouseEvent) {
+        if(this.gameState == 'aiming') {
+        
+            //get array with x,y (mouse position)
+            let pos = this.getMousePos(this.canvas, e);
+            
+            //get the angle from a certain point to the mouse in degrees
+            let mouseangle = this.radToDeg(Math.atan2(this.player.y - pos.y + 50, this.player.x - pos.x + 50))
+
+            //making sure you can't shoot down
+            if (mouseangle < 0) {
+                mouseangle = 180 + (180 + mouseangle);
+            }
+
+            //making sure you can only shoot UP
+            var lbound = 8;
+            var ubound = 172;
+            if (mouseangle > 90 && mouseangle < 270) {
+                // Left
+                if (mouseangle > ubound) {
+                    mouseangle = ubound;
+                }
+            } else {
+                // Right
+                if (mouseangle < lbound || mouseangle >= 270) {
+                    mouseangle = lbound;
+                }
+            }
+
+            //adding mouse angle to player angle
+            this.player.angle = mouseangle
+        }
+    }
+
+    // Get the touch position
+    private getTouchPos(canvas : HTMLElement, e : TouchEvent) {
+        let rect = canvas.getBoundingClientRect();
+        return {
+            x: Math.round((e.touches[0].clientX - rect.left)/(rect.right - rect.left)*canvas.clientWidth),
+            y: Math.round((e.touches[0].clientY - rect.top)/(rect.bottom - rect.top)*canvas.clientHeight)
+        }
+    }
+
     // Get the mouse position
     private getMousePos(canvas : HTMLElement, e : MouseEvent) {
         let rect = canvas.getBoundingClientRect();
@@ -108,6 +154,16 @@ export class bubbleShooter {
         }
     }
 
+    //SHOOTINGG!!
+    private onTouchEnd(e : TouchEvent) {
+        //making sure tile only moves when clicked
+        this.speed = 10
+
+        //when clicked change gamestate so tile moves lineair
+        this.gameState = 'shoot'
+    }
+
+    //SHOOTINGG!!
     private onMouseClick(e : MouseEvent) {
         //making sure tile only moves when clicked
         this.speed = 10
@@ -117,6 +173,12 @@ export class bubbleShooter {
     }
 
 public update() {
+
+        if (this.gameState == 'aiming') {
+            //rotating the tile facing to the mouse
+            this.playerDiv.style.transform = `rotate(${this.degToRad(this.player.angle)}rad)`
+        }
+
         //calculating the movement of the tile
         this.player.x += this.speed * -1*Math.cos(this.degToRad(this.player.angle))
         this.player.y += this.speed * -1*Math.sin(this.degToRad(this.player.angle))
@@ -125,7 +187,11 @@ public update() {
         this.playerDiv.style.left = `${this.player.x}px`
         this.playerDiv.style.top = `${this.player.y}px`
 
-
+        if (this.player.x < -100|| this.player.x > window.innerWidth || this.player.y < -100 || this.player.y > window.innerHeight) {
+            this.playerDiv.remove()
+            this.createTile()
+            this.speed = 0
+        }
     }
 
 }
--- docs/js/classes/GameObj/bubbleShooter.js
+++ docs/js/classes/GameObj/bubbleShooter.js
@@ -4,15 +4,19 @@ export class bubbleShooter {
         this.gameState = 'init';
         console.log('Created bubble shooter');
         document.addEventListener('mousemove', (e) => this.onMouseMove(e));
+        document.addEventListener('mousedown', (e) => this.onMouseMove(e));
         document.addEventListener('click', (e) => this.onMouseClick(e));
+        document.addEventListener('touchmove', (e) => this.onTouchMove(e));
+        document.addEventListener('touchstart', (e) => this.onTouchMove(e));
+        document.addEventListener('touchend', (e) => this.onTouchEnd(e));
         this.canvas = document.createElement('canvas');
-        this.canvas.style.height = '99vh';
-        this.canvas.style.width = '99vw';
+        this.canvas.style.height = '100vh';
+        this.canvas.style.width = '100vw';
+        this.canvas.style.position = 'fixed';
         document.body.appendChild(this.canvas);
         this.createTile();
     }
     createTile() {
-        this.gameState = 'playing';
         this.player = this.getPlayerPos();
         this.playerDiv = document.createElement('div');
         this.playerDiv.style.backgroundColor = 'red';
@@ -24,12 +28,13 @@ export class bubbleShooter {
         this.playerDiv.innerText = '<<<<<<<<<';
         this.playerDiv.style.color = 'white';
         document.body.appendChild(this.playerDiv);
+        this.gameState = 'aiming';
     }
     getPlayerPos() {
         return {
             x: window.innerWidth / 2 - 50,
             y: window.innerHeight - 100,
-            angle: 0
+            angle: 90
         };
     }
     radToDeg(angle) {
@@ -38,11 +43,10 @@ export class bubbleShooter {
     degToRad(angle) {
         return angle * (Math.PI / 180);
     }
-    onMouseMove(e) {
-        if (this.gameState == 'playing') {
-            this.playerDiv.style.transform = `rotate(${this.degToRad(this.player.angle)}rad)`;
-            let pos = this.getMousePos(this.canvas, e);
-            let mouseangle = this.radToDeg(Math.atan2(this.player.y - pos.y, this.player.x - pos.x));
+    onTouchMove(e) {
+        if (this.gameState == 'aiming') {
+            let pos = this.getTouchPos(this.canvas, e);
+            let mouseangle = this.radToDeg(Math.atan2(this.player.y - pos.y + 50, this.player.x - pos.x + 50));
             if (mouseangle < 0) {
                 mouseangle = 180 + (180 + mouseangle);
             }
@@ -61,6 +65,35 @@ export class bubbleShooter {
             this.player.angle = mouseangle;
         }
     }
+    onMouseMove(e) {
+        if (this.gameState == 'aiming') {
+            let pos = this.getMousePos(this.canvas, e);
+            let mouseangle = this.radToDeg(Math.atan2(this.player.y - pos.y + 50, this.player.x - pos.x + 50));
+            if (mouseangle < 0) {
+                mouseangle = 180 + (180 + mouseangle);
+            }
+            var lbound = 8;
+            var ubound = 172;
+            if (mouseangle > 90 && mouseangle < 270) {
+                if (mouseangle > ubound) {
+                    mouseangle = ubound;
+                }
+            }
+            else {
+                if (mouseangle < lbound || mouseangle >= 270) {
+                    mouseangle = lbound;
+                }
+            }
+            this.player.angle = mouseangle;
+        }
+    }
+    getTouchPos(canvas, e) {
+        let rect = canvas.getBoundingClientRect();
+        return {
+            x: Math.round((e.touches[0].clientX - rect.left) / (rect.right - rect.left) * canvas.clientWidth),
+            y: Math.round((e.touches[0].clientY - rect.top) / (rect.bottom - rect.top) * canvas.clientHeight)
+        };
+    }
     getMousePos(canvas, e) {
         let rect = canvas.getBoundingClientRect();
         return {
@@ -68,15 +101,27 @@ export class bubbleShooter {
             y: Math.round((e.clientY - rect.top) / (rect.bottom - rect.top) * canvas.clientHeight)
         };
     }
+    onTouchEnd(e) {
+        this.speed = 10;
+        this.gameState = 'shoot';
+    }
     onMouseClick(e) {
         this.speed = 10;
         this.gameState = 'shoot';
     }
     update() {
+        if (this.gameState == 'aiming') {
+            this.playerDiv.style.transform = `rotate(${this.degToRad(this.player.angle)}rad)`;
+        }
         this.player.x += this.speed * -1 * Math.cos(this.degToRad(this.player.angle));
         this.player.y += this.speed * -1 * Math.sin(this.degToRad(this.player.angle));
         this.playerDiv.style.left = `${this.player.x}px`;
         this.playerDiv.style.top = `${this.player.y}px`;
+        if (this.player.x < -100 || this.player.x > window.innerWidth || this.player.y < -100 || this.player.y > window.innerHeight) {
+            this.playerDiv.remove();
+            this.createTile();
+            this.speed = 0;
+        }
     }
 }
 //# sourceMappingURL=bubbleShooter.js.map
