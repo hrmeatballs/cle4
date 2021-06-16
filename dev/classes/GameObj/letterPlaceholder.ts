778a3986502e3716e8847cb0ff33729856ff1408
nigelritfeld <nigelritfeld@live.nl>
2021-06-16 17:35:32 +0200

    Created placeholder for letters in pop up menu

---
+++ dev/classes/GameObj/letterPlaceholder.ts
@@ -0,0 +1,51 @@
+export class letterPlaceholder {
+
+    private placeholder: HTMLElement;
+    private letters: Array<string>;
+    private word: any;
+
+    constructor(letters: Array<string>) {
+        this.letters = letters
+        this.init()
+        console.log(letters)
+    }
+    init()
+    {
+        this.placeholder = document.createElement('div')
+        this.placeholder.classList.add('letters-placeholder')
+        console.log(this.placeholder)
+        this.createLetters(this.letters)
+    }
+    createLetters(letters: any)
+    {
+        let createdLetters = []
+
+        for(let letter of letters)
+        {
+            let element = document.createElement('h1')
+            let content = document.createTextNode(letter)
+            element.appendChild(content)
+            element.classList.add('letter')
+            element.dataset.letter = letter
+            createdLetters.push(element)
+        }
+
+        this.setLetters(createdLetters)
+    }
+    setLetters(letters: Array<HTMLElement>)
+    {
+        let wrapper = document.createElement('div')
+        wrapper.classList.add('letters-wrapper')
+        
+        for(let element of letters)
+        {
+            wrapper.append(element)
+        }
+
+        this.placeholder.append(wrapper)
+    }
+    getElement()
+    {
+        return this.placeholder
+    }
+}
