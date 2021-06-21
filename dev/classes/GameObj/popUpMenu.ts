244256392b1b36287fdd3697af8f45451b17ccb0
Lars Beukenkamp <lars@beukenkamp.net>
2021-06-21 12:55:01 +0200

    updated score display
    Instead of showing the amount of achieved stars, now shows all 3 stars and only the achieved stars are colored.

--- dev/classes/GameObj/popUpMenu.ts
+++ dev/classes/GameObj/popUpMenu.ts
@@ -97,5 +97,11 @@ export class popUpMenu
             element.classList.add('star')
             this.stars.append(element)
         }
+        for (let i = 0; i < 3 - score; i++) {
+            let element = document.createElement('img')
+            element.src = '../docs/img/level-score-star.svg'
+            element.classList.add('star', 'locked')
+            this.stars.append(element)
+        }
     }
 }
